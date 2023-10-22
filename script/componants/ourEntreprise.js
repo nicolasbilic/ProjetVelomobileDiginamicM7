0939555b7a02109d5dd318a3e3da847e588eb3dc
zKittyGB <a-hebert@hotmail.fr>
2023-10-22 14:58:48 +0200

    start of featurs create profil

--- script/componants/ourEntreprise.js
+++ script/componants/ourEntreprise.js
@@ -2,53 +2,6 @@
 import ManageDom from "./ManageDom.js";
 import PresentationSection from "./ourEntreprise/PresentationSection.js";
 
-//data used to create the instance
-const lightBoxData = {
-  title: "Découvez le vélomobile français",
-  lightbox: [
-    "red_yellow_bike",
-    "red_bike",
-    "purple_bike",
-    "orange_velo",
-    "orange_bike",
-    "green_bike",
-  ],
-  lightboxText: {
-    number: 2,
-    title: ["Lorem ipsum dolor", "Lorem ipsum indolor"],
-
-    text: [
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pellentesque ante vitae ipsum mollis tristique in quis lacus. Vestibulum ut facilisis magna, sit amet mollis ipsum. Duis ornare tempus lorem eu maximus. Suspendisse purus augue, condimentum vel mattis vitae, ullamcorper eget leo. Fusce ut pharetra tellus. Nam egestas nibh metus,",
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pellentesque ante vitae ipsum mollis tristique in quis lacus. Vestibulum ut facilisis magna, sit amet mollis ipsum. Duis ornare tempus lorem eu maximus. Suspendisse purus augue, condimentum vel mattis vitae, ullamcorper eget leo. Fusce ut pharetra tellus. Nam egestas nibh metus,",
-    ],
-  },
-};
-
-const actualityData = {
-  nb_articles: 5,
-  pictures: [
-    "red_bike",
-    "purple_bike",
-    "orange_velo",
-    "orange_bike",
-    "green_bike",
-  ],
-  title: [
-    "Lorem indolore",
-    "Lorem indolore",
-    "Lorem indolore",
-    "Lorem indolore",
-    "Lorem indolore",
-  ],
-  text: [
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pellentesque ante vitae ipsum mollis tristique in quis lacus. Vestibulum ut facilisis magna, sit amet mollis ipsum. Duis ornare tempus lorem eu maximus. Suspendisse purus augue, condimentum vel mattis vitae, ullamcorper eget leo. Fusce ut pharetra tellus. Nam egestas nibh metus,",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pellentesque ante vitae ipsum mollis tristique in quis lacus. Vestibulum ut facilisis magna, sit amet mollis ipsum. Duis ornare tempus lorem eu maximus. Suspendisse purus augue, condimentum vel mattis vitae, ullamcorper eget leo. Fusce ut pharetra tellus. Nam egestas nibh metus,",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pellentesque ante vitae ipsum mollis tristique in quis lacus. Vestibulum ut facilisis magna, sit amet mollis ipsum. Duis ornare tempus lorem eu maximus. Suspendisse purus augue, condimentum vel mattis vitae, ullamcorper eget leo. Fusce ut pharetra tellus. Nam egestas nibh metus,",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pellentesque ante vitae ipsum mollis tristique in quis lacus. Vestibulum ut facilisis magna, sit amet mollis ipsum. Duis ornare tempus lorem eu maximus. Suspendisse purus augue, condimentum vel mattis vitae, ullamcorper eget leo. Fusce ut pharetra tellus. Nam egestas nibh metus,",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pellentesque ante vitae ipsum mollis tristique in quis lacus. Vestibulum ut facilisis magna, sit amet mollis ipsum. Duis ornare tempus lorem eu maximus. Suspendisse purus augue, condimentum vel mattis vitae, ullamcorper eget leo. Fusce ut pharetra tellus. Nam egestas nibh metus,",
-  ],
-};
-
 //Class to create a landing page
 export default class OurEntreprise extends ManageDom {
   constructor() {
--- script/componants/ourEntreprise/presentationSection.js
+++ script/componants/ourEntreprise/presentationSection.js
@@ -4,10 +4,21 @@ import ManageDom from "./../ManageDom.js";
 export default class PresentationSection extends ManageDom {
   constructor() {
     super();
-    this.render();
+    this.init();
+  }
+  //Defer the rend after initialiation
+  async init() {
+    const data_loaded = await this.fetchData();
+    this.profilDatas = data_loaded;
+    if (this.profilDatas) {
+      //call the render method
+      this.render();
+    }
   }
   //Render Method
   render() {
+    // //Call the createSection
+    const section = this.createSection();
     //Create the main container
     const main = this.createMarkup("main", "", document.body, [
       {
@@ -19,5 +30,105 @@ export default class PresentationSection extends ManageDom {
     const header = document.querySelector("header");
     //Put the body after the header
     document.body.insertBefore(main, header.nextSibling);
+    this.createPorfil(section, "valentin", "normal");
+    this.createPorfil(section, "tchoupi", "reverse");
+  }
+  //Create basic dom Element
+  createSection() {
+    //get the main container
+    const main = document.querySelector("main");
+    //create the <section>
+    const section = this.createMarkup("section", "", main, [
+      {
+        style:
+          "width: 90%; margin-bottom: 60px; display: flex; flex-direction: column; align-items: center;",
+      },
+    ]);
+    return section;
+  }
+
+  createPorfil(section, profil, order) {
+    //filter the good profil in data
+    const profilDataFiltered = Object.keys(this.profilDatas).reduce(
+      (result, key) => {
+        if (key === profil) {
+          result = this.profilDatas[key];
+        }
+        return result;
+      },
+      null
+    );
+
+    console.log("p", profilDataFiltered);
+
+    //Create the profil container
+    const containerProfil = this.createMarkup("div", "", section, [
+      {
+        style:
+          "width:80%; display:flex;  display:flex; align-items:center; justify-content: center;",
+      },
+    ]);
+    //Create the profil picture
+    const profilPicture = this.createMarkup("img", "", containerProfil, [
+      {
+        style:
+          " width:30%; height:auto;margin: 0; border-radius: 50%; background-color:black; object-fit: conain;",
+        src: `./../../assets/profil/${profil}.png`,
+        alt: `${profil}`,
+      },
+    ]);
+    //Reverse if order = reverse
+    if (order === "reverse") {
+      containerProfil.style.flexDirection = "row-reverse";
+    }
+    //Create the profil container
+    const detailsContainer = this.createMarkup("div", "", containerProfil, [
+      {
+        style:
+          "width:70%; height:auto; background-color: #EFEFEF; border-radius: 10px;",
+      },
+    ]);
+    console.log(this.profilDatas);
+
+    const profilName = this.createMarkup(
+      "h2",
+      profilDataFiltered.name,
+      detailsContainer,
+      [
+        {
+          style:
+            "width:70%; height:auto; background-color: #EFEFEF; border-radius: 10px;",
+        },
+      ]
+    );
+    const profilStatus = this.createMarkup(
+      "h2",
+      profilDataFiltered.status,
+      detailsContainer,
+      [
+        {
+          style:
+            "width:70%; height:auto; background-color: #EFEFEF; border-radius: 10px;",
+        },
+      ]
+    );
+    const profilText = this.createMarkup(
+      "p",
+      profilDataFiltered.details,
+      detailsContainer,
+      [
+        {
+          style:
+            "width:70%; height:auto; background-color: #EFEFEF; border-radius: 10px;",
+        },
+      ]
+    );
+  }
+
+  async fetchData() {
+    //importation des données du json
+    const response = await fetch("./../script/datas/ourEntreprise.json");
+    const profilDatas = await response.json();
+    return profilDatas;
   }
 }
