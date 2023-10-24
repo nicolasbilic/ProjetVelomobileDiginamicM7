ad9061974a1280be8751f25b4df1c83da518ddf8
zKittyGB <a-hebert@hotmail.fr>
2023-10-24 13:19:39 +0200

    add leafleat to contact page

---
+++ script/componants/mapLeaflet.js
@@ -0,0 +1,41 @@
+//call the map
+var map = L.map("map").setView([48.8566, 2.3522], 6);
+//init of element
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+// Array of each city
+var cities = [
+  { name: "Agde", lat: 43.3134787, lon: 3.4771629 },
+  { name: "Salon-de-Provence", lat: 43.6405237, lon: 5.0980225 },
+  { name: "Saint-Nazaire", lat: 47.2733517, lon: -2.2138905 },
+  { name: "Cassis.", lat: 43.2140359, lon: 5.5396318 },
+];
+
+// Function that connect to the api
+function getAirQuality(city) {
+  var apiKey = "b9896fb0e0956a70f0dadc9664a6478f47d50942"; //Token
+  var apiUrl = `https://api.waqi.info/feed/geo:${city.lat};${city.lon}/?token=${apiKey}`;
+  //Call the fetch
+  fetch(apiUrl)
+    //wait the resolve
+    .then((response) => response.json())
+    //stock data
+    .then((data) => {
+      var airQuality = data.data.aqi;
+      //Set up the pins
+      var airQualityIcon = L.divIcon({
+        iconSize: [30, 30],
+      });
+      // create the pins
+      L.marker([city.lat, city.lon]).addTo(map);
+    })
+    .catch((error) => console.error("Erreur:", error));
+}
+
+//loop to each city
+cities.forEach((city) => {
+  getAirQuality(city);
+});
