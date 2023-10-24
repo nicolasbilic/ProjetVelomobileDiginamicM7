3a3f623e8aac610d57dc8485d7d1f68f614811fb
Cyril <cyril.personne@outlook.fr>
2023-10-24 19:25:20 +0200

    💄 Fix border of nav position

--- script/componants/header.js
+++ script/componants/header.js
@@ -10,7 +10,7 @@ export default class Header extends ManageDom {
     const header = this.createMarkup("header", "", document.body, [
       {
         style:
-          "margin-bottom: 50px; width:100%; height: 335px; display:flex; flex-direction: column; align-items: center;  background-image: url('./../../assets/imgs/background.jpg'); background-size: cover; background-position: center; background-repeat: no-repeat; margin-top: 0px;",
+          "margin-bottom: 50px; width:100%; height: 335px; display:flex; flex-direction: column; align-items: center;  background-image: url('./../../assets/imgs/background.jpg'); background-size: cover; background-position: center; background-repeat: no-repeat; margin-top: 0px; position: relative;",
       },
     ]);
     document.body.insertBefore(header, document.body.firstChild);
@@ -20,7 +20,7 @@ export default class Header extends ManageDom {
         style:
           " width:100%; height:335px; position: absolute; display:flex; flex-direction: column; align-items: center; margin-top: 0px; background-color: black; opacity:0.65; z-index: 0;",
       },
-      {class: "filter"}
+      { class: "filter" }
     ]);
 
     const headLogo = this.createMarkup("img", "", header, [
@@ -28,7 +28,7 @@ export default class Header extends ManageDom {
       { width: "150" },
       { height: "auto" },
       { style: "z-index: 0;" },
-      { class: "headLogo"}
+      { class: "headLogo" }
     ]);
 
     const divGif = this.createMarkup("div", "", header, [
@@ -46,7 +46,7 @@ export default class Header extends ManageDom {
         style:
           "position: relative; left:10% ; bottom: -10px; animation: velomobile 8s infinite ease-in-out;",
       },
-      {class: "gif"}
+      { class: "gif" }
     ]);
 
     const hr = this.createMarkup("hr", "", header, [
@@ -58,22 +58,22 @@ export default class Header extends ManageDom {
         style:
           "margin-bottom:50px ;width:80%; display: flex; flex-direction: row; justify-content: space-around; z-index: 0; font-family: epilogue; font-size: 20px;",
       },
-      {class : "nav"}
+      { class: "nav" }
     ]);
 
     const accueil = this.createMarkup("a", "Accueil", nav, [
       { href: "../../index.html" },
-      { style: "text-decoration: none; color: white; z-index: 0;" },{class: "accueil h-link"}
+      { style: "text-decoration: none; color: white; z-index: 0;" }, { class: "accueil h-link" }
     ]);
 
     const company = this.createMarkup("a", "Notre entreprise", nav, [
       { href: "../../html/ourEntreprise.html" },
-      { style: "text-decoration: none; color: white; z-index: 0;" },{class: "h-link"}
+      { style: "text-decoration: none; color: white; z-index: 0;" }, { class: "h-link" }
     ]);
 
     const factory = this.createMarkup("a", "Notre usine", nav, [
       { href: "../../html/usine.html" },
-      { style: "text-decoration: none; color: white; z-index: 0;" },{class: "h-link"}
+      { style: "text-decoration: none; color: white; z-index: 0;" }, { class: "h-link" }
     ]);
 
     const velomobiles = this.createMarkup(
@@ -82,13 +82,13 @@ export default class Header extends ManageDom {
       nav,
       [
         { href: "../../html/velomobiles.html" },
-        { style: "text-decoration: none; color: white; z-index: 0;" },{class: "h-link"}
+        { style: "text-decoration: none; color: white; z-index: 0;" }, { class: "h-link" }
       ]
     );
 
     const contact = this.createMarkup("a", "Contact", nav, [
       { href: "../../html/contact.html" },
-      { style: "text-decoration: none; color: white; z-index: 0;" },{class: "h-link"}
+      { style: "text-decoration: none; color: white; z-index: 0;" }, { class: "h-link" }
     ]);
 
     const title = this.createMarkup("span", "Vélomobiles français", header, [
@@ -96,8 +96,8 @@ export default class Header extends ManageDom {
         style:
           "font-size: 22px; color: white; z-index: 0; font-family: epilogue;",
       },
-      {class: "head-title"}
-      
+      { class: "head-title" }
+
     ]);
 
     const flag = this.createMarkup("div", "", header, [
@@ -108,15 +108,15 @@ export default class Header extends ManageDom {
     ]);
 
     const blue = this.createMarkup("div", "", flag, [
-      { style: "width: 100px; height: 10px; background-color : #007dcc" }, { class: "flag-color"}
+      { style: "width: 100px; height: 10px; background-color : #007dcc" }, { class: "flag-color" }
     ]);
 
     const white = this.createMarkup("div", "", flag, [
-      { style: "width: 100px; height: 10px; background-color : white" },{ class: "flag-color"}
+      { style: "width: 100px; height: 10px; background-color : white" }, { class: "flag-color" }
     ]);
 
     const red = this.createMarkup("div", "", flag, [
-      { style: "width: 100px; height: 10px; background-color : #cc0000" },{ class: "flag-color"}
+      { style: "width: 100px; height: 10px; background-color : #cc0000" }, { class: "flag-color" }
     ]);
 
     const bottomDiv = this.createMarkup("div", "", header, [
@@ -124,25 +124,25 @@ export default class Header extends ManageDom {
         style:
           "width: 100%; display: flex; align-items: flex-start; justify-content: flex-start;",
       },
-      
+
     ]);
 
     const contactButton = this.createMarkup("a", "Nous contacter", bottomDiv, [
       {
         style:
-          "position: relative; bottom: -25px; width: 230px ; height: 40px; font-size: 20px; color: white; background-color: #cc0000; border: none; text-decoration: none; text-align: center; items-align: center; line-height: 40px; left:5%; font-family: epilogue",
+          "position: absolute; bottom: 10px; width: 230px ; height: 40px; font-size: 20px; color: white; background-color: #cc0000; border: none; text-decoration: none; text-align: center; items-align: center; line-height: 40px; left:5%; font-family: epilogue",
       },
       { href: "../../html/contact.html" },
-      { class: "contactButton"}
+      { class: "contactButton" }
 
     ]);
 
     const border = this.createMarkup("div", "", header, [
       {
         style:
-          "position: relative; bottom: -25px; width: 100%; height: 20px; background-color: #007dcc;",
+          "position: absolute; bottom: 0; width: 100%; height: 20px; background-color: #007dcc;",
       },
-      { class: "border"}
+      { class: "border" }
     ]);
   }
 }
