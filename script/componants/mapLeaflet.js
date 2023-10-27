eac79dee79f5eb319929b5a2660fabdc57c487d7
zKittyGB <a-hebert@hotmail.fr>
2023-10-27 12:25:32 +0200

    Update mapLeaflet.js

--- script/componants/mapLeaflet.js
+++ script/componants/mapLeaflet.js
@@ -1,6 +1,6 @@
 function Leafleat() {
   //call the map
-  var map = L.map("map").setView([48.8566, 2.3522], 6);
+  const map = L.map("map").setView([48.8566, 2.3522], 6);
   const mapClass = document.querySelector("#map").classList;
   let cities = [];
   let includeBinary = false;
@@ -30,17 +30,17 @@ function Leafleat() {
 
   // Function that connect to the api
   function getAirQuality(city) {
-    var apiKey = "b9896fb0e0956a70f0dadc9664a6478f47d50942"; //Token
-    var apiUrl = `https://api.waqi.info/feed/geo:${city.lat};${city.lon}/?token=${apiKey}`;
+    const apiKey = "b9896fb0e0956a70f0dadc9664a6478f47d50942"; //Token
+    const apiUrl = `https://api.waqi.info/feed/geo:${city.lat};${city.lon}/?token=${apiKey}`;
     //Call the fetch
     fetch(apiUrl)
       //wait the resolve
       .then((response) => response.json())
       //stock data
       .then((data) => {
-        var airQuality = data.data.aqi;
+        const airQuality = data.data.aqi;
         //Set up the pins
-        var airQualityIcon = L.divIcon({
+        const airQualityIcon = L.divIcon({
           iconSize: [30, 30],
         });
         // create the pins
